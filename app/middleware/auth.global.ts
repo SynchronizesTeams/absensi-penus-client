ec9b0c01a4e7a7004b5dc91ae81bc62d2d9ebd58
adityakurnias <adityakurniasaputra903@gmail.com>
2025-08-05 21:58:13 +0700

    auth

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,9 +1,7 @@
-import { useCookie } from "#app";
-
 export default defineNuxtRouteMiddleware((to, from) => {
   const token = useCookie("token").value;
-  if (!token) {
-    return navigateTo("/auth/login");
+  if (!token && to.path !== '/auth/login') {
+    return navigateTo('/auth/login')
   }
   
   if (to.path === "/auth/login" && token) {
