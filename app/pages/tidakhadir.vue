7d00d386c5bfcd8768135cc6f7536497b1845e13
adityakurnias <adityakurniasaputra903@gmail.com>
2026-01-27 13:02:33 +0700

    fix: tidak hadir page not showing

--- app/pages/tidakhadir.vue
+++ app/pages/tidakhadir.vue
@@ -10,8 +10,8 @@
 
     <template v-else>
       <div class="p-4 space-y-4">
-        <IzinCard @success="handleSuccess" @error="handleError" />
-        <InfoCard />
+        <CardIzinCard @success="handleSuccess" @error="handleError" />
+        <CardInfoCard />
       </div>
     </template>
   </div>
