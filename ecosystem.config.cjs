f71f1ca686fb86c336d90a4349d06a1eeda34978
adityakurnias <adityakurniasaputra903@gmail.com>
2026-01-27 09:58:52 +0700

    port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "Trabsen",
-      port: "3000",
+      port: "3005",
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
